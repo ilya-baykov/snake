7c8eab9e4d9b98fd51b88bd680d42fcde0fb06fa
ilyab <i-baykov@internet.ru>
2023-08-19 15:41:51 +0300

    Реализовал движение змейки

--- main.py
+++ main.py
@@ -3,23 +3,29 @@ import time
 import pygame
 
 pygame.init()
-size_block = 80
-WIGHT = size_block * 10
-HEIGHT = size_block * 10
-screen = pygame.display.set_mode((WIGHT, HEIGHT))
+size_block = 50
+WIDTH = size_block * 20
+HEIGHT = size_block * 15
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Змейка")
 clock = pygame.time.Clock()
-DARK_GREEN = (167, 252, 0)
+DARK_GREEN = (68, 148, 74)
 LIGHT_GREEN = (24, 255, 74)
 
-for i in range(10):
-    for j in range(10):
-        if (i + j) % 2 == 0:
-            pygame.draw.rect(screen, DARK_GREEN, (i * size_block, j * size_block, size_block, size_block))
-        else:
-            pygame.draw.rect(screen, LIGHT_GREEN, (i * size_block, j * size_block, size_block, size_block))
 
-pygame.display.update()
+def draw_field():
+    for i in range(20):
+        for j in range(15):
+            if (i + j) % 2 == 0:
+                pygame.draw.rect(screen, DARK_GREEN, (i * size_block, j * size_block, size_block, size_block))
+            else:
+                pygame.draw.rect(screen, LIGHT_GREEN, (i * size_block, j * size_block, size_block, size_block))
+    pygame.display.update()
+
+
+x = int(WIDTH / 2) + size_block / 2
+y = int(HEIGHT / 2)
+speed = 5
 
 running = True
 while running:
@@ -27,5 +33,20 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        x -= speed
+    elif keys[pygame.K_RIGHT]:
+        x += speed
+    elif keys[pygame.K_UP]:
+        y -= speed
+    elif keys[pygame.K_DOWN]:
+        y += speed
+
+    draw_field()
+    pygame.draw.circle(screen, (255, 0, 0), (x, y), 25)
+    pygame.display.update()
     clock.tick(30)
 print("Конец игры")
