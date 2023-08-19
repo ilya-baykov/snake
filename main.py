c23aca62e4a834e11de66bc866fffbb90e9fd3a4
ilyab <i-baykov@internet.ru>
2023-08-19 15:01:08 +0300

    Создал игровое поле с шахматными ячейками

--- main.py
+++ main.py
@@ -3,11 +3,23 @@ import time
 import pygame
 
 pygame.init()
-WIGHT = 1000
-HEIGHT = 800
+size_block = 80
+WIGHT = size_block * 10
+HEIGHT = size_block * 10
 screen = pygame.display.set_mode((WIGHT, HEIGHT))
 pygame.display.set_caption("Змейка")
 clock = pygame.time.Clock()
+DARK_GREEN = (167, 252, 0)
+LIGHT_GREEN = (24, 255, 74)
+
+for i in range(10):
+    for j in range(10):
+        if (i + j) % 2 == 0:
+            pygame.draw.rect(screen, DARK_GREEN, (i * size_block, j * size_block, size_block, size_block))
+        else:
+            pygame.draw.rect(screen, LIGHT_GREEN, (i * size_block, j * size_block, size_block, size_block))
+
+pygame.display.update()
 
 running = True
 while running:
